637b77d4840f2e4864a179cd6b88b53425ccb0c0
theo-bnts <theo2bont@gmail.com>
2022-03-12 18:25:06 +0100

    feat: backend for the main activity

--- app/src/main/java/fr/bnts/mentalcalculation/MainActivity.java
+++ app/src/main/java/fr/bnts/mentalcalculation/MainActivity.java
@@ -2,13 +2,41 @@ package fr.bnts.mentalcalculation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        Button playButton = findViewById(R.id.button_play);
+        playButton.setOnClickListener(view -> openGameActivity());
+
+        Button scoresButton = findViewById(R.id.button_scores);
+        scoresButton.setOnClickListener(view -> openScoresActivity());
+
+        Button aboutButton = findViewById(R.id.button_about);
+        aboutButton.setOnClickListener(view -> openAboutActivity());
     }
+
+    private void openGameActivity() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    private void openScoresActivity() {
+        Intent intent = new Intent(this, ScoresActivity.class);
+        startActivity(intent);
+    }
+
+    private void openAboutActivity() {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
+
 }
